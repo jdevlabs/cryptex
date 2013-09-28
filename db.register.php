a99ac0b013e7d27fe42efb6ceac8ba2e99b59d1a
Shadab Zafar <shadab.softs@gmail.com>
2013-09-28 23:43:32 +0530

    Teaser Trailer
    Copied some code from codrops. Can be used as a teaser for the game.
    Will push it to server.

--- db.register.php
+++ db.register.php
@@ -7,10 +7,11 @@ if(isset($_POST['username'], $_POST['name'], $_POST['password']))
 	$pass = $_POST['password'];
 	// echo $uName; echo $name; echo $pass;
 
-	// Check whether the user already exists
-	$result  = mysql_query("SELECT count(userid) FROM user WHERE username = '$uName'");
+	// Check whether the user already exist
+	// $result  = mysql_query("SELECT count(userid) FROM user WHERE username = '$uName'");
+	$result  = mysql_query("SELECT pass FROM user WHERE username = '$uName'");
 	if ( mysql_num_rows($result) > 0 )
-		die("User Already Exists");
+		die("User Already Exists $uName");
 
 	//Insert User
 	$result =  mysql_query("INSERT INTO user (username, pass) VALUES('$uName', '$pass')");
