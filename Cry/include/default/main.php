c47e8e136dc3c8685d0d2ee1825250817fec76b4
hammad haleem <hammadhaleem@gmail.com>
2013-11-19 20:26:54 +0530

    Default page

--- Cry/include/default/main.php
+++ Cry/include/default/main.php
@@ -1,1 +1,17 @@
-<h1>Default web page</h1>
+ <div class="container">
+  <div class="row">
+    <div class="span8 offset2">
+      <img  style="margin-top: 50px;" src="assets/img/Cryptex_logo.png">
+    </div>
+  </div>
+ <div class="row">
+    <div class="span11 offset1" style="margin-top: 125px;">
+      <form class="form-inline" id="submitForm">
+        <label>Let's Login Hackerboy! </label>
+        <input style="margin-left: 15px;" class="span4" type="text" id="uname" name="username" placeholder="Your Username" required autofocus>
+        <input style="margin-left: 15px;"class="span4" type="text" id="upass" name="password" placeholder="Your Password" required>
+        <button style="margin-left: 15px;"type="submit" class="btn btn-primary">Sign in</button>
+      </form>
+    </div>
+  </div>
+  </div>
---
+++ Cry/include/nav.php
@@ -0,0 +1,9 @@
+
+  <div class="navbar"><div class="navbar-inner"><div class="container-fluid">
+  <a class="brand" href="/cryptex/" name="top">Cryptex 2</a>
+  <div class="nav-collapse collapse">
+    <ul class="nav">
+      <li class="divider-vertical"></li><li><a href="/cryptex/"><i class="icon-home"></i> Home</a></li>
+      <li class="divider-vertical"></li><li><a href="#"><i class="icon-list"></i> Leaderboard</a></li>
+    </ul>
+  </div></div></div></div>
