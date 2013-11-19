37e06ca8fb37073d4d32aa3b46bc05109f335f62
hammad haleem <hammadhaleem@gmail.com>
2013-11-19 22:13:17 +0530

    Minor bug in login

--- Cry/include/login/main.php
+++ Cry/include/login/main.php
@@ -1,6 +1,6 @@
 <?php include "_login.php"; ?>
 
-<?php if ($_SESSION['loggedin'] == 1 ) { ?>
+<?php if ($_SESSION['loggedin'] != 1 ) { ?>
    <div class="span4 offset4 well"  style="margin-top: 95px;">
     <form name="input" action="?pid=login" method="POST">
 Username: <input type="text" name="user"><br/>
