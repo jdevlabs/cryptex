22c3d730f1219f1aa98688206eabfb8e122a36b3
rootavish <avishkar_gupta@outlook.com>
2014-02-02 19:26:39 +0530

    solved modal dialog refresh problem
    in the previous commit , i stated that the rules modal was showing on
    every refersh and every wrong submit . i did so the only way i know ,
    using cookies . i set a cookie to check if the uesr has visited this
    page or not . may be crude , but since i am not saving any login info
    i believe my solution is good .

--- include/rules.php
+++ include/rules.php
@@ -1,13 +1,46 @@
 <?php  
     if($_SESSION['level']==1) { ?>
       <script>
-      $(document).ready(function() { 
-		    $('#rulesModal').modal('show');
-		    $('#rulesModal').on('show.bs.modal', function()
-		     {
-		        $("#rulesModal").focus();
-		     }
-		  )
-	  });
+        function setCookie(cname,cvalue,exdays)
+		{
+			var d = new Date();
+			d.setTime(d.getTime()+(exdays*24*60*60*1000));
+			var expires = "expires="+d.toGMTString();
+			document.cookie = cname + "=" + cvalue + "; " + expires;
+		}
+
+		function getCookie(cname)
+		{
+			var name = cname + "=";
+			var ca = document.cookie.split(';');
+			for(var i=0; i<ca.length; i++) 
+			  {
+				  var c = ca[i].trim();
+				  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+		   	  }
+			return "";
+		}
+
+		function checkCookie()
+		{
+			var visit=getCookie("visited");
+			
+			if (visit == "") 
+	   	    {
+	   	    	setCookie("visited","true",20);
+			    $(document).ready(
+			    function() 
+			    { 
+				    $('#rulesModal').modal('show');
+				    $('#rulesModal').on('show.bs.modal', function()
+				     {
+				        $("#rulesModal").focus();
+				     }
+					)
+		  		});
+			}
+		}
+
+		checkCookie();
       </script>
   <?php }  ?>
