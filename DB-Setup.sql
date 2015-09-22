60f566c3daa683da8ea6cd07bc9ebc1032d27731
Shadab Zafar <dufferzafar0@gmail.com>
2015-09-22 15:41:59 +0530

    Update DB Setup SQL - admin/admin combo

--- DB-Setup.sql
+++ DB-Setup.sql
@@ -1,27 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.0.9
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Mar 09, 2014 at 06:50 PM
--- Server version: 5.6.14
--- PHP Version: 5.5.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `csijmico_cryptex`
---
-
--- --------------------------------------------------------
-
+BEGIN;
 --
 -- Table structure for table `gamedata`
 --
@@ -67,8 +44,6 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`userid`, `username`, `pass`, `email`, `name`, `gender`) VALUES
-(1, 'dufferZafar', 'plok', 'dufferzafar0@gmail.com', 'Shadab Zafar', 'M');
+(1, 'admin', 'admin', 'admin@admin.com', 'Shadab Zafar', 'M');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;
